bf12b6f2140c510d171ffe0784ba64b8023cf9db
Henrique-GRamos <h.enriacolino@gmail.com>
2025-04-27 11:45:46 -0300

    [SCRUM-14] refactory: corrigindo parametro json

--- src/pages/CriarEditarSensor/CriarEditarSensor.tsx
+++ src/pages/CriarEditarSensor/CriarEditarSensor.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
-import { useNavigate, useLocation, } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./styles.scss";
 import styles_select from "../CriarEditarEstacao/styles_select";
-
+import api from "../../services/api";
+ 
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Footer from "../../components/Footer/Footer";
 import InputMelhor from "../../components/InputMelhor/InputMelhor";
@@ -12,21 +12,20 @@ import BarraCima from "../../components/BarraCima/BarraCima";
 import { IconPlus } from "@tabler/icons-react";
 import Select from "react-select";
 import Swal from "sweetalert2";
-// import { jsx } from "react/jsx-runtime";
-
+ 
 interface TipoSensor {
   id: number;
   nome: string;
   descricao: string;
 }
-
+ 
 export default function CriarEditarSensor() {
   const navigate = useNavigate();
   const location = useLocation();
   const dadosRecebidos = location.state || null;
-
+ 
   const [modoEdicao, setModoEdicao] = useState(false);
-
+ 
   const [dadosSensor, setDadosSensor] = useState({
     nome: "",
     unidade: "",
@@ -35,11 +34,11 @@ export default function CriarEditarSensor() {
     fator_conversao: "",
     offset: "",
     tipo_sensor_id: "",
-    json: ""
+    json: "" // Adicione esta linha para incluir o campo json nos dado
   });
-
+ 
   const [sensorTypes, setSensorTypes] = useState<TipoSensor[]>([]);
-
+ 
   const [errors, setErrors] = useState({
     nome: false,
     unidade: false,
@@ -49,34 +48,28 @@ export default function CriarEditarSensor() {
     offset: false,
     tipo_sensor_id: false,
     json: false
+ 
   });
-
+ 
   useEffect(() => {
-    const fetchTiposSensores = async () => {
-      try {
-        const response = await api.get("/tipo_parametros/");
-        const data = response.data;
-        const sortedData = data.sort((a: TipoSensor, b: TipoSensor) => a.nome.localeCompare(b.nome));
+    api.get("/tipo_parametros/")
+      .then(response => {
+        const sortedData = response.data.sort((a: TipoSensor, b: TipoSensor) =>
+          a.nome.localeCompare(b.nome)
+        );
         setSensorTypes(sortedData);
-      } catch (error) {
-        console.error("Erro ao buscar tipos de sensores:", error);
-      }
-    };
-  
-    fetchTiposSensores();
+      })
+      .catch(error => console.error("Erro:", error));
   }, []);
-
+ 
   const sensorTypeOptions = sensorTypes.map(sensor => ({
     value: sensor.id.toString(),
     label: sensor.nome
   }));
-
+ 
   useEffect(() => {
     if (dadosRecebidos && sensorTypes.length > 0) {
       setModoEdicao(true);
-      console.log("Dados recebidos no modo edição:", dadosRecebidos);
-      console.log("Tipos de sensor disponíveis:", sensorTypes);
-
       setDadosSensor({
         nome: dadosRecebidos.nome || "",
         unidade: dadosRecebidos.unidade || "",
@@ -86,19 +79,21 @@ export default function CriarEditarSensor() {
         offset: dadosRecebidos.offset?.toString() || "",
         tipo_sensor_id: dadosRecebidos.tipo_parametro_id?.toString() || "",
         json: dadosRecebidos.json || ""
+       
       });
     }
   }, [dadosRecebidos, sensorTypes]);  
-
+ 
   const handleInputChange = (tag: string, value: string) => {
     setDadosSensor(prev => ({ ...prev, [tag]: value }));
     setErrors(prev => ({ ...prev, [tag]: false }));
   };
-
+ 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSensorTypeSelect = (selectedOption: any) => {
     handleInputChange("tipo_sensor_id", selectedOption?.value);
   };
-
+ 
   const handleDelete = async (id: number) => {
     try {
       await api.delete(`/parametros/${id}`);
@@ -106,11 +101,10 @@ export default function CriarEditarSensor() {
       console.error("Erro ao excluir sensor:", err);
     }
   };
-  
-
+ 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
+ 
     const newErrors = {
       nome: !dadosSensor.nome,
       unidade: !dadosSensor.unidade,
@@ -120,11 +114,12 @@ export default function CriarEditarSensor() {
       offset: !dadosSensor.offset,
       tipo_sensor_id: !dadosSensor.tipo_sensor_id,
       json:!dadosSensor.json
+     
     };
-
+ 
     setErrors(newErrors);
     if (Object.values(newErrors).some(error => error)) return;
-
+ 
     const sensorData = {
       nome: dadosSensor.nome,
       unidade: dadosSensor.unidade,
@@ -135,24 +130,19 @@ export default function CriarEditarSensor() {
       tipo_parametro_id: Number(dadosSensor.tipo_sensor_id),
       json: dadosSensor.json
     };
-
+ 
     try {
-      let response;
       if (modoEdicao && dadosRecebidos?.id) {
-        response = await api.put(`/parametros/${dadosRecebidos.id}`, sensorData);
+        await api.put(`/parametros/${dadosRecebidos.id}`, sensorData);
       } else {
-        response = await api.post("/parametros/", sensorData);
+        await api.post("/parametros/", sensorData);
       }
-
-      if (response.ok) {
-        Swal.fire({
-          icon: "success",
-          title: modoEdicao ? "Sensor atualizado com sucesso!" : "Sensor cadastrado com sucesso!",
-          confirmButtonColor: "#5751D5"
-        }).then(() => navigate("/sensores"));
-      } else {
-        throw new Error("Erro ao salvar sensor");
-      }
+ 
+      Swal.fire({
+        icon: "success",
+        title: modoEdicao ? "Sensor atualizado com sucesso!" : "Sensor cadastrado com sucesso!",
+        confirmButtonColor: "#5751D5"
+      }).then(() => navigate("/sensores"));
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       Swal.fire({
@@ -161,9 +151,8 @@ export default function CriarEditarSensor() {
         text: error?.response?.data?.detail || error.message || "Erro desconhecido",
         confirmButtonColor: "#ED3C5C"
       });
-    }    
+    }
   };
-
   return (
     <div className="pagina_wrapper">
       <Sidebar />
@@ -172,8 +161,8 @@ export default function CriarEditarSensor() {
           <BarraCima
             nome={modoEdicao ? "Editar Sensor" : "Criar Sensor"}
             tipo="voltar"
-            entidade="Sensor"
-            onDelete={() => handleDelete(dadosRecebidos.id)}
+            entidade={modoEdicao ? "Sensor" : undefined}
+            onDelete={modoEdicao ? () => handleDelete(dadosRecebidos.id) : undefined}
           />
           <form onSubmit={handleSubmit}>
             <div className="secao_input cima">
@@ -186,7 +175,7 @@ export default function CriarEditarSensor() {
                 mostrarErro={errors.nome}
                 placeholder="Nome do sensor"
               />
-
+ 
               <InputMelhor
                 label="Unidade"
                 tag="unidade"
@@ -197,7 +186,7 @@ export default function CriarEditarSensor() {
                 placeholder="Unidade do sensor (Ex: °C, %)"
               />
             </div>
-
+ 
             <div className="secao_input cima">
               <InputMelhor
                 label="Descrição"
@@ -209,7 +198,7 @@ export default function CriarEditarSensor() {
                 placeholder="Descrição do sensor"
               />
             </div>
-
+ 
             <div className="secao_input cima">
               <InputMelhor
                 label="Quantidade de Casas Decimais"
@@ -221,7 +210,7 @@ export default function CriarEditarSensor() {
                 mostrarErro={errors.quantidade_casas_decimais}
                 placeholder="Número de casas decimais"
               />
-
+ 
               <InputMelhor
                 label="Fator de Conversão"
                 tag="fator_conversao"
@@ -232,7 +221,7 @@ export default function CriarEditarSensor() {
                 mostrarErro={errors.fator_conversao}
                 placeholder="Fator de conversão"
               />
-
+ 
               <InputMelhor
                 label="Offset"
                 tag="offset"
@@ -243,10 +232,10 @@ export default function CriarEditarSensor() {
                 mostrarErro={errors.offset}
                 placeholder="Offset"
               />
-
+ 
               <InputMelhor
                 label="JSON"
-                tag="offset"
+                tag="json"
                 width={33}
                 type="string"
                 value={dadosSensor.json}
@@ -255,7 +244,7 @@ export default function CriarEditarSensor() {
                 placeholder="JSON"
               />
             </div>
-
+ 
             <div className="secao_input cima">
               <p className="ceal_escrito">Tipo de Sensor</p>
               <Select
@@ -266,7 +255,7 @@ export default function CriarEditarSensor() {
                 classNamePrefix="id"
                 styles={styles_select}
               />
-
+ 
               <BotaoCTA
                 aparencia="secundario"
                 cor="cor_primario"
@@ -277,7 +266,7 @@ export default function CriarEditarSensor() {
               />
             </div>
             {errors.tipo_sensor_id && <p className="input_erro">Preencha este campo.</p>}
-
+ 
             <div className="cima80">
               <BotaoCTA
                 aparencia="primario"
@@ -294,3 +283,4 @@ export default function CriarEditarSensor() {
     </div>
   );
 }
+ 
